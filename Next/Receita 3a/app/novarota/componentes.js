fabcedc6efdbd0312fe3fc533f5332b316b362ff
luizmiguel4444 <luizmiguel4444@gmail.com>
2025-01-16 23:38:21 -0300

    Receita 3a de Next finalizada

---
+++ Next/Receita 3a/app/novarota/componentes.js
@@ -0,0 +1,15 @@
+export function MariaPrea() {
+  return <h2>Morreu Maria Preá...</h2>;
+}
+
+export function JoseGamba() {
+  return <h2>Morreu José Gambá...</h2>;
+}
+
+export function FuncParam({ texto }) {
+  return (
+    <div>
+      <h2>{texto}</h2>
+    </div>
+  );
+}
